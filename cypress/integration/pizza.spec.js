e632121a4b654103d7103bf838bae20e61d7c564
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-08-21 17:19:21 -0400

    Got two of the tests done, working on getting the dropdown menu value

--- cypress/integration/pizza.spec.js
+++ cypress/integration/pizza.spec.js
@@ -1,7 +1,28 @@
 describe('Pizza form tester', () => {
     describe('Testing typing in the box', () => {
         it('can naviagate to pizza website "http://localhost:3000/pizza"', () => {
-            crypto.visit('http://localhost:3000/pizza');
+            cy.visit('http://localhost:3000/pizza');
+        })
+
+        it('can add text to the box', () => {
+            cy.get('input[name="specialInstructions"]')
+            .type('Special Instructions test')
+            .should('have.value', 'Special Instructions test')
+        })
+
+        it('can select multiple toppings', () => {
+            cy.get('input[name="pepperoni"]').click()
+            cy.get('input[name="mushroom"]').click()
+        })
+
+        it('can submit the form', () => {
+            cy.get('input[name="name"]')
+            .type('Eli')
+
+            cy.get('input[value="original red"]').click()
+            
+            cy.get('[name="size"]')
+            //cy.select('select[value="large"]')
         })
     })
 
