58ea8b0afd433d0f096041a0b2336f0689873fd7
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-08-21 16:55:09 -0400

    Finishing cypress

---
+++ cypress/integration/pizza.spec.js
@@ -0,0 +1,8 @@
+describe('Pizza form tester', () => {
+    describe('Testing typing in the box', () => {
+        it('can naviagate to pizza website "http://localhost:3000/pizza"', () => {
+            crypto.visit('http://localhost:3000/pizza');
+        })
+    })
+
+})
